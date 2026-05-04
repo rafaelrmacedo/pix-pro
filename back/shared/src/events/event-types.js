6621d56f6d7c10eb6a84e4b8fad3644ecaf4217a
Rafael Macedo <rafael.macedo@jala.university>
2026-05-04 10:25:38 -0300

    refact: type module

--- back/shared/src/events/event-types.js
+++ back/shared/src/events/event-types.js
@@ -1,8 +1,6 @@
-const EVENT_TYPES = {
+export const EVENT_TYPES = {
   IMAGE_UPLOADED: "image.uploaded",
   IMAGE_PROCESSED: "image.processed",
   PROCESSING_ERROR: "image.error",
   NOTIFICATION_STATUS_UPDATED: "notification.status.updated"
 };
-
-module.exports = { EVENT_TYPES };
